9ff3b52cdc20925e92a2dde6b835237e64d286b5
vikdema <viktordeminoff@gmail.com>
2020-10-16 22:07:47 +0600

    p2

--- Demin/lab1/Source/main.cpp
+++ Demin/lab1/Source/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
 
+//indents
 static int k = 0;
+//for file "result.txt"
 static std::ofstream fout;
 
-
+//recursive function
 long f(long n) {
     fout << "f(" << n << ")::\\";
     fout << "\n";
@@ -14,17 +16,19 @@ long f(long n) {
         for (int i = 0; i < k; i++) {
             fout << "\t";
         }
-        fout <<"/f(" << n << ")::"<<1;
+        fout << "/f(" << n << ")::" << 1;
         fout << "\n";
 
         return 1;
     }
     if (n >= 2) {
         for (long i = 2; i <= n; i++) {
+
             k++;
             for (int i = 0; i < k; i++) {
                 fout << "\t";
             }
+
             sum += f(n / i);
 
             k--;
@@ -34,17 +38,17 @@ long f(long n) {
         for (int i = 1; i <= k; i++) {
             fout << "\t";
         }
-        fout <<"/f(" << n << ")::"<<sum;
+        fout << "/f(" << n << ")::" << sum;
         fout << "\n";
         return sum;
     }
     return 0;
 }
 
-void print_result() {
-    fout.open("result.txt");
+//result output
+void print_result(int a, int b) {
     long n = 0;
-    for (int i = 15; i < 31; ++i) {
+    for (int i = a; i < b; ++i) {
         fout << "f(" << i << ") : " << "\n";
         fout << "-------------------------------------------------------------------" << "\n";
         n = f(i);
@@ -52,8 +56,34 @@ void print_result() {
     }
 }
 
+//type_selection
+void type_selection() {
+    int type = 0;
+    int value = 0;
+    std::cout << "select type:\n 1-one value,2 - from 15 to 31\n";
+    std::cin >> type;
+    switch (type) {
+        case 1:
+            std::cout << "enter value\n";
+            std::cin >> value;
+            if(value<1){
+                std::cout<<"Incorrect data";
+                break;
+            }
+            print_result(value, value + 1);
+            break;
+        case 2:
+            print_result(15, 31);
+            break;
+        default:
+            std::cout << "error: wrong type";
+    }
+    std::cout << "intermediate values are written to the result file";
+}
+
 int main() {
-    print_result();
+    fout.open("result.txt");
+    type_selection();
     fout.close();
     return 0;
 }
