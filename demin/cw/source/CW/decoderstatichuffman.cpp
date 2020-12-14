f82a5ef1ebcdbaa512a3477d44eb3efe03adb046
vikdema <viktordeminoff@gmail.com>
2020-12-14 19:56:01 +0300

    add_tests

--- demin/cw/source/CW/decoderstatichuffman.cpp
+++ demin/cw/source/CW/decoderstatichuffman.cpp
@@ -6,32 +6,31 @@ DecoderStaticHuffman::DecoderStaticHuffman()
 
 vector<pair<char, string>> DecoderStaticHuffman::decoding(string &str,vector<pair<char, string>> al) {
     QElapsedTimer clock;
-    clock.start();
-    full_code.clear();
-    size_t n=0;
-       for(auto it = str.begin();it<str.end();it++){
-           for (auto i = al.begin(); i <al.end() ; ++i) {
-                   while(n<i->second.size()&& *it==i->second[n]){
-                       it++;
-                       if(it==str.end()){
-                           break;
-                       }
-                       n++;
-                   }
-                   if(n==i->second.size()){
-                       it--;
-                       n=0;
-                       full_code+=i->first;
-                       break;
-                   }
-           }
-    }
-    string result;
-    result+= std::to_string(str.size())+" "+std::to_string(full_code.size()*8)+" "+std::to_string(al.size())+" "+std::to_string(clock.elapsed())+'\n';
-    notify(result);
-    return al;
-}
+        clock.start();
+        full_code.clear();
+        size_t n=0;
+           for(auto it = str.begin();it<str.end();it++){
+               for (auto i = al.begin(); i <al.end() ; ++i) {
+                   n=0;
+                       while(n<i->second.size()&& *it==i->second[n]){
+                           it++;
 
+                           n++;
+                       }
+                       if(n==i->second.size()){
+                           it--;
+                           full_code+=i->first;
+                           break;
+                       }else{
+                           it-=n;
+                           }
+               }
+        }
+        string result;
+        result+= std::to_string(str.size())+" "+std::to_string(full_code.size()*8)+" "+std::to_string(al.size())+" "+std::to_string(clock.elapsed())+'\n';
+        notify(result);
+        return al;
+}
 string DecoderStaticHuffman::get_code() {
     return full_code;
 }
@@ -44,4 +43,5 @@ void DecoderStaticHuffman::notify(string message){
     file->write(message.c_str());
     delete file;
 }
+
 
